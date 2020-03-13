50d3e175ae06a832ada1c55dc490b4f009f5107f
Montana Flynn <montana949@gmail.com>
2020-03-14 00:31:24 +0700

    Add doc.go for showing description and example on pkg.go.dev

---
+++ size/doc.go
@@ -0,0 +1,28 @@
+/*
+Package size is for determining dimensions
+of media without decoding the entire file.
+
+Example Usage:
+
+```go
+giphy := "https://media1.giphy.com/media/l0ErxFClZX9L3bgBi/giphy.gif"
+res, err := http.Get(giphy)
+if err != nil {
+	log.Fatal(err)
+}
+defer res.Body.Close()
+size, err := size.Parse(res.Body)
+if err != nil {
+	log.Fatal(err)
+}
+jsonBytes, err := json.Marshal(size)
+if err != nil {
+	log.Fatal(err)
+}
+fmt.Printf("%s", jsonBytes)
+// {"Width":480,"Height":270,"ImageType":"GIF"}
+```
+
+MIT License Copyright (c) 2020 Montana Flynn (https://montanaflynn.com)
+*/
+package size
